5f8047ba10429d49cebb97d308737ae0036e1f99
root <root@errr>
2010-01-23 03:08:14 -0600

    version .02 Removed getopts cleaned up the way this work

--- nagger.py
+++ nagger.py
@@ -1,103 +1,54 @@
 #!/usr/bin/env python
-"""Nagger to use with Nagios to notify via Twitter
 
-Usage: nagger [options]
-
-Required Options:
-    if:
-      --dmonly                    This is used to only direct message the list of users
-                                  Can not be used with --tweetit
-      --users=...                 Comma seperated list of twitter
-                                  bob,tim,jack
-    if:
-      --tweetit                   This will just post the msg to your timeline for all followers to see
-                                  This option can not be used with --dmonly
-    --msg=...                     This should be in the form: --msg="my message goes here"
-                                  This message can not be empty, or > 140 Chars (strict)
-                                  This app will not wrap your post to make multiple tweets
-    --twitusr=...                 Username of twitter user
-    --twitpass=...                Password for the twitter account
-"""
 __author__ = "Michael Rice"
-__version__ = "0.1"
-__date__ = "2010-01-22"
-__license__ = "Apache"
+__version__ = "0.2"
+__date__ = "2010-01-23"
+__license__ = "Apache2"
 
-
-
-import twitter
-import getopt
-import os
-import sys
+import twitter,sys
+from optparse import OptionParser
 
 class nagger:
-
   def __init__(self,uname="test",passwd="test"):
       self.user = uname
       self.passwd = passwd
       self.connect = twitter.Api(username=self.user,password=self.passwd)
   
-  def inform(self,users,msg):
+  def inform(self,user,msg):
       '''Methos to send direct message to a list of users about foo'''
-      if type(users).__name__ != 'list':
-          return False
       if len(msg) <= 0 or len(msg) >= 141:
           return False
-      # now we know we have a list and a valid length message lets loop though and 
-      # send the message to each user
-      for user in users:
-          self.connect.PostDirectMessage(user, msg)
+      self.connect.PostDirectMessage(user, msg)
 
   def tweetit(self,msg):
       '''Method to just tweet the nagios status'''
       if len(msg) <= 0 or len(msg) >= 141:
           return False
-
       self.connect.PostUpdate(msg)
 
-def main(argv):
-    dmonly = False
-    tweetit = False
-    users = []
-    msg = ''
-    twitusr = ''
-    twitpass = ''
-
-    try:
-        opts,args = getopt.getopt(argv, 'h',['help','users=','twitusr=','twitpass=','dmonly','tweetit','msg='])
-    except getopt.GetoptError:
-        usage()
+def main():
+    usage = "usage: %prog [options]"
+    version="%prog .02"
+    parser = OptionParser()
+    parser.add_option('-u', '--user', action="store", dest="user",
+        help="Set the user name for the twitter account who will get the message")
+    parser.add_option('-t', '--twitusr', action="store",dest="twitusr",
+        help="Twitter account to auth as. Default is to use values in the config file")
+    parser.add_option("-p", "--twitpass",dest="twitpass",help="Password for Twitter user")
+    parser.add_option('-T','--tweetit',action="store_true",dest="tweetit",
+        help="Use this option to post to the users status. Do not use with -d or --dmonly")
+    parser.add_option("-d","--dmonly",action="store_false", dest="tweetit",
+        help="Use this option to direct message a user. Must be used with -u or --user")
+    parser.add_option("-m","--msg",dest="msg",help="140 char max len message to be sent to twitter")
+    (options, args) = parser.parse_args()
+    twitcon = nagger(options.twitusr,options.twitpass)
+    if not options.tweetit and options.user:
+        twitcon.inform(options.user,options.msg)
+    elif options.tweetit:
+        twitcon.tweetit(options.msg)
+    else:
+        print "You are missing some options. Try the -h flag for help"
         sys.exit(2)
-    for opt,arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif opt == '--dmonly':
-            dmonly = True
-            tweetit = False
-        elif opt == '--tweetit':
-            dmonly = False
-            tweetit = True
-        elif opt == '--users':
-            users = arg.split(',')
-        elif opt == '--twitusr':
-            twitusr = arg
-        elif opt == '--twitpass':
-            twitpass = arg
-        elif opt == '--msg':
-            if len(arg) <= 0 or len(arg) >= 141:
-                usage()
-                sys.exit()
-            msg = arg
-        
-    twitcon = nagger(twitusr,twitpass)
-    if dmonly and not tweetit:
-        twitcon.inform(users,msg)
-    if tweetit and not dmonly:
-        twitcon.tweetit(msg)
-
-def usage():
-    print __doc__
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
